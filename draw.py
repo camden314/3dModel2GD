0684cfdaee2de5e04b83bf1767d7dff79a9252ca
Camden314 <camden7306@gmail.com>
2020-11-19 09:07:31 -0600

    make code look better

--- draw.py
+++ draw.py
@@ -1,78 +1,54 @@
-#import matplotlib.pyplot as plt
 import numpy as np
 import random
-#import matplotlib.pyplot as plt
-from render import * 
+from render import *
 import os.path
 import sys
 
 try:
-	blend_data = np.load(os.path.join(os.path.expanduser('~'),'.blend.npz'), allow_pickle=True)
+    blend_data = np.load(os.path.join(os.path.expanduser('~'), '.blend.npz'),
+                         allow_pickle=True)
 except FileNotFoundError:
-	sys.stderr.write("\033[0;31m[Error] Make sure to run the blender script before the drawing script\033[0m\n")
-	exit(1)
+    sys.stderr.write("\033[0;31m[Error] Make sure to run the blender script"
+                     "before the drawing script\033[0m\n")
+    exit(1)
 
 faces = blend_data['arr_0']
 camera_mat = blend_data['arr_1']
 inverse = np.linalg.inv(camera_mat)
-def to2D(x,y,z, width=7, height=12):
-	vector = np.array([[x],[y],[z], [1]])
-	step1 = inverse @ vector
 
-	x,y,z,w = step1[0,0], step1[1,0], step1[2,0], step1[3,0]
 
-	x/=w
-	y/=w
-	z/=w
+def to2D(x, y, z, width=7, height=12):  # Project 3d points onto a 2d plane
+    vector = np.array([[x], [y], [z], [1]])
+    step1 = inverse @ vector
 
-	x = (x+1)*width/2.
-	y = (y+1)*height/2.
-	return np.array([x,y])
+    x, y, z, w = step1[0, 0], step1[1, 0], step1[2, 0], step1[3, 0]
+
+    x /= w
+    y /= w
+    z /= w
+
+    x = (x + 1) * width / 2.
+    y = (y + 1) * height / 2.
+    return np.array([x, y])
 
 tris = []
-colors = []
-faces = faces.tolist()
-faces.sort(key=lambda x:x[0][2])
-faces.reverse()
 
-faces = np.array(faces)
 for face in faces:
-	tri = []
-	_12dist = abs(face[1][2]-face[2][2])
-	_10dist = abs(face[1][2]-face[0][2])
-	colors.append((_12dist+_10dist)/3.)
-	for indx in range(3):
-		verts = face[indx]
-		tri.append(to2D(*verts.tolist()[:3]))
-	tris.append(tri)
+    tri = []
+    for indx in range(3):
+        verts = face[indx]
+        tri.append(to2D(*verts.tolist()[:3]))
+    tris.append(tri)
 
 tris = np.array(tris)
-
-print("lol")
-print(len(tris))
-c = 0
-
-a = np.array(colors)
-colors = (150*(a - np.min(a))/np.ptp(a)).astype(int)+70
-
 test = level.Level("test")
-
 pogs = level.Transformer()
+c = 0
 for tri in tris:
-	#if c>800:
-	#	break
-
-	#plt.scatter(*tri.T, s=1)
-
-	mult = 255-colors[c]
-	hexcol = '#%02x%02x%02x' % (round(mult),round(mult),round(mult))
-
-	#pol = plt.Polygon(tri)
-	pogs.appendTransformer(Triformer.fromPoints((tri*4000)))
-	#plt.gca().add_patch(pol)
-	c+=1
+    pogs.appendTransformer(Triformer.fromPoints((tri * 4000)))
+    c += 1
 
 pogs.scale(0.25) \
-	.moveTo(x=500, y=500) \
-	.addToLevel(test)
-msgport.uploadToGD(test,per=7000)
+    .moveTo(x=500, y=500) \
+    .addToLevel(test)
+msgport.uploadToGD(test, per=7000)
--- render.py
+++ render.py
@@ -1,172 +1,195 @@
-import level
-import msgport
-import numpy as np
-from level import Block
+"""
+Render file.
+
+its the render part
+"""
+
 from math import acos, hypot, sqrt
 
-def centeroid(arr):
-	length = arr.shape[0]
-	sum_x = np.sum(arr[:, 0])
-	sum_y = np.sum(arr[:, 1])
-	return sum_x/length, sum_y/length
+import level
 
-def getDistance(p1,p2):
-	dist = sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
-	return dist
+from level import Block
+
+import msgport
+
+import numpy as np
+
+
+def centeroid(arr):
+    length = arr.shape[0]
+    sum_x = np.sum(arr[:, 0])
+    sum_y = np.sum(arr[:, 1])
+    return sum_x / length, sum_y / length
+
+
+def getDistance(p1, p2):
+    dist = sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
+    return dist
+
 
 class Triformer(level.Transformer):
-	def __init__(self, *blocks):
-		super().__init__(*blocks)
-		self.lineLength = 0
+    def __init__(self, *blocks):
+        super().__init__(*blocks)
+        self.lineLength = 0
 
-		self.triA = 0
-		self.triB = 0
-		self.triC = 0
+        self.triA = 0
+        self.triB = 0
+        self.triC = 0
 
-		self.triab = 0
+        self.triab = 0
 
-	def rotateBeginning(self, degrees=90, modifier=1):
-		if modifier==-1:
-			self.blocks.reverse()
-		piv = (self.blocks[0].coords()[0]-(14.6*modifier), self.blocks[0].coords()[1])
+    def rotateBeginning(self, degrees=90, modifier=1):
+        if modifier == -1:
+            self.blocks.reverse()
+        piv = self.blocks[0].coords()
+        piv[0] -= (14.6 * modifier)
 
-		self.rotate(degrees, pivot=piv)
-		if modifier==-1:
-			self.blocks.reverse()
-		return self
+        self.rotate(degrees, pivot=piv)
+        if modifier == -1:
+            self.blocks.reverse()
+        return self
 
-	@classmethod
-	def createLine(cls, x, y, length, end=False):
-		blocks = []
-		x+=15
-		if end:
-			x-=length
-		fullturns = int(length//30)
-		for i in range(fullturns):
-			blocks.append(Block(blockid=579, x_position=x+(30*i), y_position=y))
+    @classmethod
+    def createLine(cls, x, y, length, end=False):
+        blocks = []
+        x += 15
+        if end:
+            x -= length
+        fullturns = int(length // 30)
+        for i in range(fullturns):
+            blocks.append(Block(blockid=579, x_position=x + (30 * i),
+                                y_position=y))
+        if length % 30 != 0:
+            x += (30 * (fullturns - 1) + (length % 30))
+            blocks.append(Block(blockid=579, x_position=x, y_position=y))
+        if end:
+            blocks.reverse()
+            pass
+        retvalue = cls(*blocks)
+        retvalue.lineLength = length
+        return retvalue
 
-		if length%30!=0:
-			blocks.append(Block(blockid=579, x_position=x+(30*(fullturns-1)+(length%30)), y_position=y))
-		
-		if end:
-			blocks.reverse()
-			pass
-		retvalue = cls(*blocks)
-		retvalue.lineLength = length
-		return retvalue
+    def attachLineAtStart(self, length, clear=False):
+        piv = list(self.blocks[0].coords())
 
-	def attachLineAtStart(self, length, clear=False):
-		piv = list(self.blocks[0].coords())
+        piv[0] -= np.cos(np.deg2rad(float(self.blocks[0]['rotation']))) * 15
+        piv[1] += np.sin(np.deg2rad(float(self.blocks[0]['rotation']))) * 15
 
-		piv[0] -= np.cos(np.deg2rad(float(self.blocks[0]['rotation'])))*15
-		piv[1] += np.sin(np.deg2rad(float(self.blocks[0]['rotation'])))*15
+        line = Triformer.createLine(piv[0], piv[1], length, end=False)
 
-		line = Triformer.createLine(piv[0], piv[1], length, end=False)
+        if clear:
+            self.clear()
+        self.appendTransformer(line)
+        self.lineLength = line.lineLength
+        return self
 
-		if clear:
-			self.clear()
-		self.appendTransformer(line)
-		self.lineLength = line.lineLength
-		return self
+    def attachLineAtEnd(self, length, clear=False):
+        piv = list(self.blocks[-1].coords())
 
-	def attachLineAtEnd(self, length, clear=False):
-		piv = list(self.blocks[-1].coords())
+        piv[0] += np.cos(np.deg2rad(float(self.blocks[-1]['rotation']))) * 15
+        piv[1] -= np.sin(np.deg2rad(float(self.blocks[-1]['rotation']))) * 15
 
-		piv[0] += (np.cos(np.deg2rad(float(self.blocks[-1]['rotation'])))*15)
-		piv[1] -= (np.sin(np.deg2rad(float(self.blocks[-1]['rotation'])))*15)
+        line = Triformer.createLine(piv[0], piv[1], length, end=False)
 
-		line = Triformer.createLine(piv[0], piv[1], length, end=False)
+        if clear:
+            self.clear()
+        self.appendTransformer(line)
+        self.lineLength = line.lineLength
+        return self
 
-		if clear:
-			self.clear()
-		self.appendTransformer(line)
-		self.lineLength = line.lineLength
-		return self
+    def findTriCenter(self):
+        start = (self.blocks[0].coords()[0] - 14.6, self.blocks[0].coords()[1])
 
-	def findTriCenter(self):
-		start = (self.blocks[0].coords()[0]-14.6, self.blocks[0].coords()[1])
+        pointA = start
 
-		pointA = start
+        pointC = [start[0] + self.triA, start[1]]
 
-		pointC = [start[0]+self.triA, start[1]]
+        pointB = [start[0], start[1]]
 
-		pointB = [start[0], start[1]]
+        pointB[0] += np.cos(np.deg2rad(self.triab)) * (self.triB - 0.6)
+        pointB[1] -= np.sin(np.deg2rad(self.triab)) * (self.triB - 0.6)
 
-		pointB[0] += np.cos(np.deg2rad(self.triab))*(self.triB-0.6)
-		pointB[1] -= np.sin(np.deg2rad(self.triab))*(self.triB-0.6)
+        return centeroid(np.array([pointA, pointB, pointC]))
 
-		return centeroid(np.array([pointA,pointB,pointC]))
+    def scaleTri(self, X=2, origin='d'):
+        if origin == 'd':
+            Px, Py = self.findTriCenter()
+        else:
+            Px, Py = origin
+        for b in self.blocks:
+            Ax, Ay = b.coords()
 
-	def scaleTri(self, X=2, origin='d'):
-		if origin=='d':
-			Px, Py = self.findTriCenter()
-		else:
-			Px, Py = origin
-		for b in self.blocks:
-			Ax, Ay = b.coords()
+            sx = X * (Ax - Px) + Px
+            sy = X * (Ay - Py) + Py
 
-			sx = X * (Ax - Px) + Px
-			sy = X * (Ay - Py) + Py
+            if b['size'] == '0':
+                b['size'] = 1
+            print(b['size'])
+            b['size'] = float(b['size']) * X
+            b.setCoords(sx, sy)
+        return self
 
-			if b['size']=='0':
-				b['size']=1
-			print(b['size'])
-			b['size'] = float(b['size'])*X
-			b.setCoords(sx,sy)
-		return self
+    def fillTri(self):
+        x1, y1 = self.findTriCenter()
+        x2, y2 = self.blocks[0].coords()
+        x2 -= 14.6
 
-	def fillTri(self):
-		x1, y1 = self.findTriCenter()
-		x2, y2 = (self.blocks[0].coords()[0]-14.6, self.blocks[0].coords()[1])
-		dist = int((hypot(x2 - x1, y2 - y1)//4)+2)
+        dist = int((hypot(x2 - x1, y2 - y1) // 4) + 2)
 
-		i=1
-		while min(self.triA, self.triB, self.triC)/i>30:
-			tr = self.__class__.createTri(x2, y2, self.triA/i, self.triB/i, self.triC/i)
+        i = 1
+        while min(self.triA, self.triB, self.triC) / i > 30:
+            x3 = (1 / i) * (x2 - (x2 + self.triA)) + self.triA
 
-			x3 = (1/i)*(x2 - (x2+self.triA)) + self.triA
-			tr2 = self.__class__.createTri(x3+x2, y2, self.triA/i, self.triB/i, self.triC/i)
-			self.appendTransformer(tr).appendTransformer(tr2)
-			i+=0.25/dist
-		return self
+            tr = Triformer.createTri(x2, y2, self.triA / i, self.triB / i,
+                                     self.triC / i)
+            tr2 = Triformer.createTri(x3 + x2, y2, self.triA / i,
+                                      self.triB / i, self.triC / i)
 
-	@classmethod
-	def createTri(cls, x, y, A=30, B=40, C=50):
-		try:
-			ab = np.rad2deg(acos((A * A + B * B - C * C)/(2.0 * A * B)))
-			ac = np.rad2deg(acos((A * A + C * C - B * B)/(2.0 * A * C)))
-		except ValueError:
-			raise ValueError("Invalid triangle with A=%d B=%d C=%d"%(A,B,C))
+            self.appendTransformer(tr).appendTransformer(tr2)
+            i += 0.25 / dist
+        return self
 
-		triA = cls.createLine(x, y, A)
-		triB = triA.duplicate().attachLineAtStart(B, clear=True).rotateBeginning(ab)
-		triC = triA.duplicate().attachLineAtEnd(C, clear=True).move(x=-1).rotateBeginning(180+(-1*ac))
-		out = triA.appendTransformer(triC).appendTransformer(triB)
+    @classmethod
+    def createTri(cls, x, y, A=30, B=40, C=50):
+        try:
+            ab = np.rad2deg(acos((A * A + B * B - C * C) / (2.0 * A * B)))
+            ac = np.rad2deg(acos((A * A + C * C - B * B) / (2.0 * A * C)))
+        except ValueError:
+            raise ValueError("Invalid triangle with A=%d B=%d C=%d" % (A, B, C))
 
-		out.triA, out.triB, out.triC = triA.lineLength, triB.lineLength, triC.lineLength
-		out.triab = ab
-		return out
+        triA = cls.createLine(x, y, A)
+        triB = triA.duplicate().attachLineAtStart(B, clear=True) \
+                               .rotateBeginning(ab)
 
-	@classmethod
-	def fromPoints(cls, vertices):
-		pointAB, pointAC, pointBC = vertices
+        triC = triA.duplicate().attachLineAtEnd(C, clear=True) \
+                               .move(x=-1) \
+                               .rotateBeginning(180 + (-1 * ac))
+        out = triA.appendTransformer(triC).appendTransformer(triB)
 
-		blks = level.Transformer(Block(blockid=916, x_position=pointAB[0],y_position=pointAB[1], size=0.2, color1=3, group_parent=1),
-								 Block(blockid=916, x_position=pointAC[0],y_position=pointAC[1], size=0.2, color1=3, group_parent=1),
-								 Block(blockid=916, x_position=pointBC[0],y_position=pointBC[1], size=0.2, color1=3, group_parent=1))
+        out.triA = triA.lineLength
+        out.triB = triB.lineLength
+        out.triC = triC.lineLength
 
-		pointAC, pointBC = pointBC, pointAC
+        out.triab = ab
+        return out
 
-		lineA = getDistance(pointAB, pointAC)
-		lineB = getDistance(pointAB, pointBC)
-		lineC = getDistance(pointAC, pointBC)
+    @classmethod
+    def fromPoints(cls, vertices):
+        pointAB, pointAC, pointBC = vertices
 
-		relativeToOrigin = pointAC-pointAB
+        pointAC, pointBC = pointBC, pointAC
 
-		dgs = np.rad2deg(np.arctan2(relativeToOrigin[1],relativeToOrigin[0]))
+        lineA = getDistance(pointAB, pointAC)
+        lineB = getDistance(pointAB, pointBC)
+        lineC = getDistance(pointAC, pointBC)
 
-		if min(lineA, lineB, lineC)>=14:
-			tri = cls.createTri(pointAB[0],pointAB[1], A=lineA, B=lineB, C=lineC).rotateBeginning(-dgs)
-		else:
-			tri = level.Transformer()
-		return tri
+        relativeToOrigin = pointAC - pointAB
+
+        dgs = np.rad2deg(np.arctan2(relativeToOrigin[1], relativeToOrigin[0]))
+
+        if min(lineA, lineB, lineC) >= 14:
+            tri = cls.createTri(pointAB[0], pointAB[1], lineA, lineB, lineC) \
+                     .rotateBeginning(-dgs)
+        else:
+            tri = level.Transformer()
+        return tri
